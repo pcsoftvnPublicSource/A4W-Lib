8083b5f1e3433acdf06d482292d3a579b3cb58b6
Cao Anh <anhct@pcsoftvn.com>
2016-01-06 16:40:56 +0700

    version 1.0
    Completed version 1.0 with 70 animation effects and document.

---
+++ js/pcsoftvn.animation.js
@@ -0,0 +1,288 @@
+$(document).ready(function(){
+    $(".slideInDown-animation-hover").hover(function(){
+    	$(this).addClass("slideInDown");}, function(){$(this).removeClass("slideInDown");});
+    $(".slideInDown-animation-click").click(function(){$(this).addClass("slideInDown");});
+
+    $(".slideInLeft-animation-hover").hover(function(){
+    	$(this).addClass("slideInLeft");}, function(){$(this).removeClass("slideInLeft");});
+    $(".slideInLeft-animation-click").click(function(){$(this).addClass("slideInLeft");});
+
+    $(".slideInRight-animation-hover").hover(function(){
+    	$(this).addClass("slideInRight");}, function(){$(this).removeClass("slideInRight");});
+    $(".slideInRight-animation-click").click(function(){$(this).addClass("slideInRight");})
+
+
+    $(".slideInUp-animation-hover").hover(function(){
+    	$(this).addClass("slideInUp");}, function(){$(this).removeClass("slideInUp");});
+    $(".slideInUp-animation-click").click(function(){$(this).addClass("slideInUp");})
+
+
+    $(".slideOutDown-animation-hover").hover(function(){
+    	$(this).addClass("slideOutDown");}, function(){$(this).removeClass("slideOutDown");});
+    $(".slideOutDown-animation-click").click(function(){$(this).addClass("slideOutDown");})
+
+
+    $(".slideOutLeft-animation-hover").hover(function(){
+    	$(this).addClass("slideOutLeft");}, function(){$(this).removeClass("slideOutLeft");});
+    $(".slideOutLeft-animation-click").click(function(){$(this).addClass("slideOutLeft");})
+
+    $(".slideOutRight-animation-hover").hover(function(){
+    	$(this).addClass("slideOutRight");}, function(){$(this).removeClass("slideOutRight");});
+    $(".slideOutRight-animation-click").click(function(){$(this).addClass("slideOutRight");})
+
+    $(".slideOutUp-animation-hover").hover(function(){
+    	$(this).addClass("slideOutUp");}, function(){$(this).removeClass("slideOutUp");});
+    $(".slideOutUp-animation-click").click(function(){$(this).addClass("slideOutUp");})
+
+    $(".fadeIn-animation-hover").hover(function(){
+    	$(this).addClass("fadeIn");}, function(){$(this).removeClass("fadeIn");});
+    $(".fadeIn-animation-click").click(function(){$(this).addClass("fadeIn");})
+
+    $(".fadeInDown-animation-hover").hover(function(){
+    	$(this).addClass("fadeInDown");}, function(){$(this).removeClass("fadeInDown");});
+    $(".fadeInDown-animation-click").click(function(){$(this).addClass("fadeInDown");})
+
+    $(".fadeInDownBig-animation-hover").hover(function(){
+    	$(this).addClass("fadeInDownBig");}, function(){$(this).removeClass("fadeInDownBig");});
+    $(".fadeInDownBig-animation-click").click(function(){$(this).addClass("fadeInDownBig");})
+
+    $(".fadeInLeft-animation-hover").hover(function(){
+    	$(this).addClass("fadeInLeft");}, function(){$(this).removeClass("fadeInLeft");});
+    $(".fadeInLeft-animation-click").click(function(){$(this).addClass("fadeInLeft");})
+
+    $(".fadeInLeftBig-animation-hover").hover(function(){
+    	$(this).addClass("fadeInLeftBig");}, function(){$(this).removeClass("fadeInLeftBig");});
+    $(".fadeInLeftBig-animation-click").click(function(){$(this).addClass("fadeInLeftBig");})
+
+    $(".fadeInRight-animation-hover").hover(function(){
+    	$(this).addClass("fadeInRight");}, function(){$(this).removeClass("fadeInRight");});
+    $(".fadeInRight-animation-click").click(function(){$(this).addClass("fadeInRight");})
+
+    $(".fadeInRightBig-animation-hover").hover(function(){
+    	$(this).addClass("fadeInRightBig");}, function(){$(this).removeClass("fadeInRightBig");});
+    $(".fadeInRightBig-animation-click").click(function(){$(this).addClass("fadeInRightBig");})
+
+    $(".fadeInUp-animation-hover").hover(function(){
+    	$(this).addClass("fadeInUp");}, function(){$(this).removeClass("fadeInUp");});
+    $(".fadeInUp-animation-click").click(function(){$(this).addClass("fadeInUp");})
+
+    $(".fadeInUpBig-animation-hover").hover(function(){
+    	$(this).addClass("fadeInUpBig");}, function(){$(this).removeClass("fadeInUpBig");});
+    $(".fadeInUpBig-animation-click").click(function(){$(this).addClass("fadeInUpBig");})
+
+    $(".replace-animation-hover").hover(function(){
+    	$(this).addClass("replace");}, function(){$(this).removeClass("replace");});
+    $(".replace-animation-click").click(function(){$(this).addClass("replace");})
+
+    $(".fadeOut-animation-hover").hover(function(){
+    	$(this).addClass("fadeOut");}, function(){$(this).removeClass("fadeOut");});
+    $(".fadeOut-animation-click").click(function(){$(this).addClass("fadeOut");})
+
+    $(".fadeOutDown-animation-hover").hover(function(){
+    	$(this).addClass("fadeOutDown");}, function(){$(this).removeClass("fadeOutDown");});
+    $(".fadeOutDown-animation-click").click(function(){$(this).addClass("fadeOutDown");})
+
+    $(".fadeOutDownBig-animation-hover").hover(function(){
+    	$(this).addClass("fadeOutDownBig");}, function(){$(this).removeClass("fadeOutDownBig");});
+    $(".fadeOutDownBig-animation-click").click(function(){$(this).addClass("fadeOutDownBig");})
+
+    $(".fadeOutLeft-animation-hover").hover(function(){
+    	$(this).addClass("fadeOutLeft");}, function(){$(this).removeClass("fadeOutLeft");});
+    $(".fadeOutLeft-animation-click").click(function(){$(this).addClass("fadeOutLeft");})
+
+    $(".fadeOutLeftBig-animation-hover").hover(function(){
+    	$(this).addClass("fadeOutLeftBig");}, function(){$(this).removeClass("fadeOutLeftBig");});
+    $(".fadeOutLeftBig-animation-click").click(function(){$(this).addClass("fadeOutLeftBig");})
+
+    $(".fadeOutRight-animation-hover").hover(function(){
+    	$(this).addClass("fadeOutRight");}, function(){$(this).removeClass("fadeOutRight");});
+    $(".fadeOutRight-animation-click").click(function(){$(this).addClass("fadeOutRight");})
+
+    $(".fadeOutRightBig-animation-hover").hover(function(){
+    	$(this).addClass("fadeOutRightBig");}, function(){$(this).removeClass("fadeOutRightBig");});
+    $(".fadeOutRightBig-animation-click").click(function(){$(this).addClass("fadeOutRightBig");})
+
+    $(".fadeOutUp-animation-hover").hover(function(){
+    	$(this).addClass("fadeOutUp");}, function(){$(this).removeClass("fadeOutUp");});
+    $(".fadeOutUp-animation-click").click(function(){$(this).addClass("fadeOutUp");})
+
+    $(".fadeOutUpBig-animation-hover").hover(function(){
+    	$(this).addClass("fadeOutUpBig");}, function(){$(this).removeClass("fadeOutUpBig");});
+    $(".fadeOutUpBig-animation-click").click(function(){$(this).addClass("fadeOutUpBig");})
+
+    $(".zoomIn-animation-hover").hover(function(){
+    	$(this).addClass("zoomIn");}, function(){$(this).removeClass("zoomIn");});
+    $(".zoomIn-animation-click").click(function(){$(this).addClass("zoomIn");})
+
+    $(".zoomInDown-animation-hover").hover(function(){
+    	$(this).addClass("zoomInDown");}, function(){$(this).removeClass("zoomInDown");});
+    $(".zoomInDown-animation-click").click(function(){$(this).addClass("zoomInDown");})
+
+    $(".zoomInLeft-animation-hover").hover(function(){
+    	$(this).addClass("zoomInLeft");}, function(){$(this).removeClass("zoomInLeft");});
+    $(".zoomInLeft-animation-click").click(function(){$(this).addClass("zoomInLeft");})
+
+    $(".zoomInRight-animation-hover").hover(function(){
+    	$(this).addClass("zoomInRight");}, function(){$(this).removeClass("zoomInRight");});
+    $(".zoomInRight-animation-click").click(function(){$(this).addClass("zoomInRight");})
+
+    $(".zoomInUp-animation-hover").hover(function(){
+    	$(this).addClass("zoomInUp");}, function(){$(this).removeClass("zoomInUp");});
+    $(".zoomInUp-animation-click").click(function(){$(this).addClass("zoomInUp");})
+
+    $(".zoomOut-animation-hover").hover(function(){
+    	$(this).addClass("zoomOut");}, function(){$(this).removeClass("zoomOut");});
+    $(".zoomOut-animation-click").click(function(){$(this).addClass("zoomOut");})
+
+    $(".zoomOutDown-animation-hover").hover(function(){
+    	$(this).addClass("zoomOutDown");}, function(){$(this).removeClass("zoomOutDown");});
+    $(".zoomOutDown-animation-click").click(function(){$(this).addClass("zoomOutDown");})
+
+    $(".zoomOutLeft-animation-hover").hover(function(){
+    	$(this).addClass("zoomOutLeft");}, function(){$(this).removeClass("zoomOutLeft");});
+    $(".zoomOutLeft-animation-click").click(function(){$(this).addClass("zoomOutLeft");})
+
+    $(".zoomOutRight-animation-hover").hover(function(){
+    	$(this).addClass("zoomOutRight");}, function(){$(this).removeClass("zoomOutRight");});
+    $(".zoomOutRight-animation-click").click(function(){$(this).addClass("zoomOutRight");})
+
+    $(".zoomOutUp-animation-hover").hover(function(){
+    	$(this).addClass("zoomOutUp");}, function(){$(this).removeClass("zoomOutUp");});
+    $(".zoomOutUp-animation-click").click(function(){$(this).addClass("zoomOutUp");})
+
+    $(".bounce-animation-hover").hover(function(){
+    	$(this).addClass("bounce");}, function(){$(this).removeClass("bounce");});
+    $(".bounce-animation-click").click(function(){$(this).addClass("bounce");})
+
+    $(".bounceIn-animation-hover").hover(function(){
+    	$(this).addClass("bounceIn");}, function(){$(this).removeClass("bounceIn");});
+    $(".bounceIn-animation-click").click(function(){$(this).addClass("bounceIn");})
+
+    $(".bounceInDown-animation-hover").hover(function(){
+    	$(this).addClass("bounceInDown");}, function(){$(this).removeClass("bounceInDown");});
+    $(".bounceInDown-animation-click").click(function(){$(this).addClass("bounceInDown");})
+
+    $(".bounceInLeft-animation-hover").hover(function(){
+    	$(this).addClass("bounceInLeft");}, function(){$(this).removeClass("bounceInLeft");});
+    $(".bounceInLeft-animation-click").click(function(){$(this).addClass("bounceInLeft");})
+
+    $(".bounceInRight-animation-hover").hover(function(){
+    	$(this).addClass("bounceInRight");}, function(){$(this).removeClass("bounceInRight");});
+    $(".bounceInRight-animation-click").click(function(){$(this).addClass("bounceInRight");})
+
+    $(".bounceInUp-animation-hover").hover(function(){
+    	$(this).addClass("bounceInUp");}, function(){$(this).removeClass("bounceInUp");});
+    $(".bounceInUp-animation-click").click(function(){$(this).addClass("bounceInUp");})
+
+    $(".bounceOut-animation-hover").hover(function(){
+    	$(this).addClass("bounceOut");}, function(){$(this).removeClass("bounceOut");});
+    $(".bounceOut-animation-click").click(function(){$(this).addClass("bounceOut");})
+
+    $(".bounceOutDown-animation-hover").hover(function(){
+    	$(this).addClass("bounceOutDown");}, function(){$(this).removeClass("bounceOutDown");});
+    $(".bounceOutDown-animation-click").click(function(){$(this).addClass("bounceOutDown");})
+
+    $(".bounceOutLeft-animation-hover").hover(function(){
+    	$(this).addClass("bounceOutLeft");}, function(){$(this).removeClass("bounceOutLeft");});
+    $(".bounceOutLeft-animation-click").click(function(){$(this).addClass("bounceOutLeft");})
+
+    $(".bounceOutRight-animation-hover").hover(function(){
+    	$(this).addClass("bounceOutRight");}, function(){$(this).removeClass("bounceOutRight");});
+    $(".bounceOutRight-animation-click").click(function(){$(this).addClass("bounceOutRight");})
+
+    $(".bounceOutUp-animation-hover").hover(function(){
+    	$(this).addClass("bounceOutUp");}, function(){$(this).removeClass("bounceOutUp");});
+    $(".bounceOutUp-animation-click").click(function(){$(this).addClass("bounceOutUp");})
+
+    $(".animated.flip-hover").hover(function(){
+    	$(this).addClass("animated.flip");}, function(){$(this).removeClass("animated.flip");});
+    $(".animated.flip-click").click(function(){$(this).addClass("animated.flip");})
+
+    $(".flipInX-animation-hover").hover(function(){
+    	$(this).addClass("flipInX");}, function(){$(this).removeClass("flipInX");});
+    $(".flipInX-animation-click").click(function(){$(this).addClass("flipInX");})
+
+    $(".flipInY-animation-hover").hover(function(){
+    	$(this).addClass("flipInY");}, function(){$(this).removeClass("flipInY");});
+    $(".flipInY-animation-click").click(function(){$(this).addClass("flipInY");})
+
+    $(".flipOutX-animation-hover").hover(function(){
+    	$(this).addClass("flipOutX");}, function(){$(this).removeClass("flipOutX");});
+    $(".flipOutX-animation-click").click(function(){$(this).addClass("flipOutX");})
+
+    $(".flipOutY-animation-hover").hover(function(){
+    	$(this).addClass("flipOutY");}, function(){$(this).removeClass("flipOutY");});
+    $(".flipOutY-animation-click").click(function(){$(this).addClass("flipOutY");})
+
+    $(".rotateIn-animation-hover").hover(function(){
+    	$(this).addClass("rotateIn");}, function(){$(this).removeClass("rotateIn");});
+    $(".rotateIn-animation-click").click(function(){$(this).addClass("rotateIn");})
+
+    $(".rotateInDownLeft-animation-hover").hover(function(){
+    	$(this).addClass("rotateInDownLeft");}, function(){$(this).removeClass("rotateInDownLeft");});
+    $(".rotateInDownLeft-animation-click").click(function(){$(this).addClass("rotateInDownLeft");})
+
+    $(".rotateInDownRight-animation-hover").hover(function(){
+    	$(this).addClass("rotateInDownRight");}, function(){$(this).removeClass("rotateInDownRight");});
+    $(".rotateInDownRight-animation-click").click(function(){$(this).addClass("rotateInDownRight");})
+
+    $(".rotateInUpLeft-animation-hover").hover(function(){
+    	$(this).addClass("rotateInUpLeft");}, function(){$(this).removeClass("rotateInUpLeft");});
+    $(".rotateInUpLeft-animation-click").click(function(){$(this).addClass("rotateInUpLeft");})
+
+    $(".rotateInUpRight-animation-hover").hover(function(){
+    	$(this).addClass("rotateInUpRight");}, function(){$(this).removeClass("rotateInUpRight");});
+    $(".rotateInUpRight-animation-click").click(function(){$(this).addClass("rotateInUpRight");})
+
+    $(".rotateOut-animation-hover").hover(function(){
+    	$(this).addClass("rotateOut");}, function(){$(this).removeClass("rotateOut");});
+    $(".rotateOut-animation-click").click(function(){$(this).addClass("rotateOut");})
+
+    $(".rotateOutDownLeft-animation-hover").hover(function(){
+    	$(this).addClass("rotateOutDownLeft");}, function(){$(this).removeClass("rotateOutDownLeft");});
+    $(".rotateOutDownLeft-animation-click").click(function(){$(this).addClass("rotateOutDownLeft");})
+
+    $(".rotateOutDownRight-animation-hover").hover(function(){
+    	$(this).addClass("rotateOutDownRight");}, function(){$(this).removeClass("rotateOutDownRight");});
+    $(".rotateOutDownRight-animation-click").click(function(){$(this).addClass("rotateOutDownRight");})
+
+    $(".rotateOutUpRight-animation-hover").hover(function(){
+    	$(this).addClass("rotateOutUpRight");}, function(){$(this).removeClass("rotateOutUpRight");});
+    $(".rotateOutUpRight-animation-click").click(function(){$(this).addClass("rotateOutUpRight");});
+
+    $(".flash-animation-hover").hover(function(){
+    	$(this).addClass("flash");}, function(){$(this).removeClass("flash");});
+    $(".flash-animation-click").click(function(){$(this).addClass("flash");});
+
+    $(".pulse-animation-hover").hover(function(){
+    	$(this).addClass("pulse");}, function(){$(this).removeClass("pulse");});
+    $(".pulse-animation-click").click(function(){$(this).addClass("pulse");})
+
+    $(".rubberBand-animation-hover").hover(function(){
+    	$(this).addClass("rubberBand");}, function(){$(this).removeClass("rubberBand");});
+    $(".rubberBand-animation-click").click(function(){$(this).addClass("rubberBand");})
+
+    $(".shake-animation-hover").hover(function(){
+    	$(this).addClass("shake");}, function(){$(this).removeClass("shake");});
+    $(".shake-animation-click").click(function(){$(this).addClass("shake");})
+
+    $(".swing-animation-hover").hover(function(){
+    	$(this).addClass("swing");}, function(){$(this).removeClass("swing");});
+    $(".swing-animation-click").click(function(){$(this).addClass("swing");})
+
+    $(".tada-animation-hover").hover(function(){
+    	$(this).addClass("tada");}, function(){$(this).removeClass("tada");});
+    $(".tada-animation-click").click(function(){$(this).addClass("tada");})
+
+    $(".wobble-animation-hover").hover(function(){
+    	$(this).addClass("wobble");}, function(){$(this).removeClass("wobble");});
+    $(".wobble-animation-click").click(function(){$(this).addClass("wobble");})
+
+    $(".lightSpeedIn-animation-hover").hover(function(){
+    	$(this).addClass("lightSpeedIn");}, function(){$(this).removeClass("lightSpeedIn");});
+    $(".lightSpeedIn-animation-click").click(function(){$(this).addClass("lightSpeedIn");})
+
+    $(".lightSpeedOut-animation-hover").hover(function(){
+    	$(this).addClass("lightSpeedOut");}, function(){$(this).removeClass("lightSpeedOut");});
+    $(".lightSpeedOut-animation-click").click(function(){$(this).addClass("lightSpeedOut");})
+});
